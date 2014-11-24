a12683ba61371e86b27a3c8189dd0ebb8a5612b8
crot4lus <glenn@glenntaylor.co.uk>
2014-11-24 17:24:40 +0000

    Create simpleTabs.js

---
+++ simpleTabs.js
@@ -0,0 +1,42 @@
+/**
+ * Creates a very basic tab interface 
+ */
+(function(){
+	'use strict';
+	
+	$.extend({
+		simpleTabs: function(options){
+			// If no tabs are present return
+			if(!$('[data-tab-name]')){ return; }
+			
+			var settings = $.extend({}, options);
+			
+			// Create the tabs container
+			var $tabs = $('<div/>', { 'class': 'simpleTabs' })
+				.insertBefore($('[data-tab-name]')
+				.first())
+				.append('<ul/>');
+			
+			// Create a reference to ul object
+			var $ul = $('ul', $tabs);
+			
+			$('[data-tab-name]').each(function(){
+				var $tab = $(this);
+				// Create tab for this item
+				$('<li/>', { 'text': $tab.data('tab-name') }).appendTo($ul).click(function(){
+					$('li', $ul).removeClass('active');
+					$('[data-tab-name]').hide();
+					$(this).addClass('active');
+					$tab.show();
+				});;
+
+				// Hide tab
+				$(this).hide();
+			});
+			
+			// Enable first tab
+			$('[data-tab-name]').first().show();
+			$('li', $ul).first().addClass('active');
+		}
+	});
+})(jQuery);
