66fc6b9bde5d523c468d7a1356b0c918b5847fe2
crot4lus <glenn@glenntaylor.co.uk>
2014-12-03 12:10:54 +0000

    Update simpleTabs.js

--- simpleTabs.js
+++ simpleTabs.js
@@ -1,50 +1,43 @@
-/**
- * Creates a very basic tab interface 
- */
-(function(){
-	'use strict';
-	
-	$.extend({
-		simpleTabs: function(options){
-			// If no tabs are present return
-			if(!$('[data-tab-name]')){ return; }
-			
-			var settings = $.extend({
-				callback: function(){}
-			}, options);
-			
-			// Create the tabs container
-			var $tabs = $('<div/>', { 'class': 'simpleTabs' })
-				.insertBefore($('[data-tab-name]')
-				.first())
-				.append('<ul/>');
-			
-			// Create a reference to ul object
-			var $ul = $('ul', $tabs);
-			
-			$('[data-tab-name]').each(function(){
-				var $tab = $(this);
-				// Create tab for this item
-				$('<li/>', { 'text': $tab.data('tab-name'), 'class': $tab.data('class-name') }).appendTo($ul).click(function(){
-					$('li', $ul).removeClass('active');
-					$('[data-tab-name]').hide();
-					$(this).addClass('active');
-					$tab.show();
-					
-					if(options.callback){
-						// call the callback and apply the scope:
-						options.callback.call(this);
-					}
-					
-				});
+$.fn.simpleTabs = function(options){
+	// If no tabs are present return
 
-				// Hide tab
-				$(this).hide();
+	var settings = $.extend({
+		callback: function(){}
+	}, options);
+
+	return this.each(function(){
+		var $container = $(this);
+		// Create the tabs container
+		var $tabs = $('<div/>', { 'class': 'simpleTabs' })
+		.insertBefore($('[data-tab-name]', $container)
+					  .first())
+		.append('<ul/>');
+
+		// Create a reference to ul object
+		var $ul = $('ul', $tabs);
+
+		$('[data-tab-name]', $container).each(function(){
+			var $tab = $(this);
+			// Create tab for this item
+			$('<li/>', { 'text': $tab.data('tab-name'), 'class': $tab.data('class-name') }).appendTo($ul).click(function(){
+				$('li', $ul).removeClass('active');
+				$('[data-tab-name]', $container).hide();
+				$(this).addClass('active');
+				$tab.show();
+
+				if(options.callback){
+					// call the callback and apply the scope:
+					options.callback.call(this);
+				}
+
 			});
-			
-			// Enable first tab
-			$('[data-tab-name]').first().show();
-			$('li', $ul).first().addClass('active');
-		}
+
+			// Hide tab
+			$(this).hide();
+		});
+
+		// Enable first tab
+		$('[data-tab-name]', $container).first().show();
+		$('li', $ul).first().addClass('active');
 	});
-})(jQuery);
+}
